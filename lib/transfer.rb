1671059f9f86f98178809b016f503319b4314791
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-30 01:14:06 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,55 +1,21 @@
-# class Transfer
-#
-#   attr_accessor :transfer, :sender, :receiver, :status, :amount
-#
-#   def initialize(sender, receiver, amount)
-#     @transfer = transfer
-#     @sender = sender
-#     @receiver = receiver
-#     @status = "pending"
-#     @amount = amount
-#   end
-#
-#   def valid?
-#     if @sender.valid? && @receiver.valid?
-#       true
-#     else
-#       false
-#     end
-#   end
-#
-#   def execute_transaction
-#     if @sender.balance > @amount && @status == "pending"
-#       @sender.balance -= @amount
-#       @receiver.balance += @amount
-#       @status = "complete"
-#     else
-#       @status = "rejected"
-#       return "Transaction rejected. Please check your account balance."
-#     end
-#   end
-#
-#   def reverse_transfer
-#     if @status == "complete"
-#       @sender.balance += @amount
-#       @receiver.balance -= @amount
-#       @status = "reversed"
-#     end
-#   end
-# end
-
 class Transfer
-  attr_accessor :sender, :receiver, :amount, :status
+
+  attr_accessor :transfer, :sender, :receiver, :status, :amount
 
   def initialize(sender, receiver, amount)
+    @transfer = transfer
     @sender = sender
     @receiver = receiver
-    @amount = amount
     @status = "pending"
+    @amount = amount
   end
 
   def valid?
-    @sender.valid? && @receiver.valid? ? true : false
+    if @sender.valid? && @receiver.valid?
+      true
+    else
+      false
+    end
   end
 
   def execute_transaction
